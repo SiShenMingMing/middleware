8023d989b9d6bb8aa5fa0be3759372c0c2490c86
崔晓亮 <cuixls@yonyou.com>
2021-08-31 13:40:49 +0800

    update role mysql

--- start_job.sh
+++ start_job.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
+#颜色 35紫色
+ECHO_COLOR=35
+
+
+# 打印眼熟
+function diyecho(){
+	echo -e "\033[0;$2m$1 \033[0m"
+}
+
+
+function usage(){
+    case $1 in
+        mysql)
+            diyecho "用法:\n start_job.sh  mysql  [选项] ... [参数] ...\n\n选项列表:" $ECHO_COLOR
+            diyecho " mysql-install [端口号]\n"  $ECHO_COLOR
+            diyecho "例子:\n start_job.sh  mysql mysql-install 3306"  $ECHO_COLOR
+        ;;
+        postgresql)
+        ;;
+        *)  
+            diyecho "作用:\n 初始化服务器及部署指定middleware\n"  $ECHO_COLOR
+            diyecho "用法:\n start_job.sh  [选项] ... [参数] ...\n\n选项列表:"  $ECHO_COLOR
+            diyecho "mysql       -- mysql工具"  $ECHO_COLOR
+            diyecho "postgresql  -- postgresql工具"  $ECHO_COLOR
+        esac
+
+}
+
 
 case $1 in
 
   mysql)
      case $2 in
       mysql-install)
-          ansible-playbook --tags=mysql-install -e "MYSQL_PORT=$3" -i inventory/hosts.ini /all.yaml
+          ansible-playbook --tags=mysql-install -v -e "MYSQL_PORT=$3" -i inventory/hosts.ini main.yaml
       ;;
      *)
-      echo "Usage: ./start_job.sh mysql [ mysql-install 3306 | mysql-uninstall 3306 | mysql-bak C0519 ] "
+      usage mysql
       exit 1
      ;;
      esac
@@ -39,8 +67,8 @@ case $1 in
      ;;
      esac
   ;;
-    *) 
-    echo "Usage: ./start_job.sh [ azq | mysql | postgresql | mongodb ] "
+  *) 
+    usage
     exit 1
   ;; 
 esac
